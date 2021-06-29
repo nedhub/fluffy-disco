157d97791953ee2ef34ada92885e7648e76f773c
Ned Ememanka <nedememanka@icloud.com>
2021-06-29 17:14:58 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".button-container").addEventListener("click", () => {
+
+    console.log("I was clicked")
+
+    let text = document.getElementById("filter-jobs").value;
+    console.log(text)
+
+
+
+
+
+
+})
